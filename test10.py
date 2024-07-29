b1bf935ba29313498f7311de05180a1aac5861b9
Surya Das <42703407+SuryaSD@users.noreply.github.com>
2024-07-29 20:22:15 +0530

    Update test10.py

--- test10.py
+++ test10.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 import getpass
-st.write(getpass.getuser())
+#st.write(getpass.getuser())
+
+# Get the username
+username = os.getlogin()
+
+# Display the username in Streamlit
+st.write(f"Hello, {username}!")
