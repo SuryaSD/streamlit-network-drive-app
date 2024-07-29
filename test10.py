e741f6707f28aa10b8d1ac724203685357322058
Surya Das <42703407+SuryaSD@users.noreply.github.com>
2024-07-29 20:20:15 +0530

    Update test10.py

--- test10.py
+++ test10.py
@@ -2,34 +2,4 @@ import streamlit as st
 import pandas as pd
 import os
 
-
-# Path to the network drive
-network_drive_path = r"\\ltssvcp2nas\Victoria\MERCH\VSL Reporting and Analytics\SAS\MSI_Data_Dump\Checking_Delete.xlsx"
-
-@st.cache_resource
-def load_data(filepath):
-    # Check if the file exists
-    if os.path.exists(filepath):
-        # Read the CSV file
-        data = pd.read_excel(filepath)
-        return data
-    else:
-        st.error("File not found!")
-        return pd.DataFrame()
-
-def main():
-    st.title("Network Drive Data Viewer")
-
-    # Load data from the network drive
-    data = load_data(network_drive_path)
-
-    if not data.empty:
-        # Display the data in a table
-        st.write("Data from the network drive:")
-        st.dataframe(data)
-    else:
-        st.write("No data available to display.")
-        
-
-if __name__ == "__main__":
-    main()
+st.write(getpass.getuser())
