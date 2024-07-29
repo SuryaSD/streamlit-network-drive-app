9d0d2305e50761f47390df23bfba83f6d0655c51
Surya Das <42703407+SuryaSD@users.noreply.github.com>
2024-07-29 20:26:35 +0530

    Update test10.py

--- test10.py
+++ test10.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import os
 import getpass
-#st.write(getpass.getuser())
-
 # Get the username from environment variable
 username = os.getenv('USERNAME')
 
 # Display the username in Streamlit
-st.write(f"Hello, {username}!")
+if username:
+    st.write(f"Hello, {username}!")
+else:
+    st.write("Username environment variable not set.")
